cb13ad56ce11ca792e9b6180a8ae2f7f5176498b
Kirillmlk <kostakov.kiirill@yandex.by>
2024-03-08 22:03:25 +0300

    add scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,6 +25,11 @@ const linkAction = () =>{
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('blur-header') : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
 
 
 
